510690a61b501eafcf7fb29f0868187bbeafc0f6
ThaoDujardin <thaodujardin@gmail.com>
2026-01-20 15:19:59 +0100

    Implementation du Lecteur de Fichiers

--- src/app/metier/LecteurFichier.java
+++ src/app/metier/LecteurFichier.java
@@ -1,6 +1,39 @@
 package app.metier;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LecteurFichier
 {
-	
+	//-----------------------
+	// Lecture du fichier
+	//-----------------------
+	public static String lireFichier( String cheminFichier ) throws IOException
+	{
+		StringBuilder  contenu = new StringBuilder();
+
+		BufferedReader br      = new BufferedReader(new FileReader(cheminFichier));
+
+		// A chaque ligne, on ajoute un espace
+		String ligne;
+		while ((ligne = br.readLine()) != null)
+			contenu.append(ligne).append(" ");
+
+		br.close();
+
+		// Nettoyage
+		contenu = new StringBuilder( LecteurFichier.nettoyerTexte( contenu.toString() ) );
+
+		return contenu.toString();
+	}
+
+	//-----------------------
+	// Nettoyage du texte
+	//-----------------------
+	public static String nettoyerTexte( String texte )
+	{
+		// Normalisation, on ne garde que les lettres et chiffres
+		return texte.toLowerCase().replaceAll("[^a-z0-9]", " ");
+	}
 }
